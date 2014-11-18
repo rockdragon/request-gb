e849851832f02b4b48a7a70551828aa66186fc8d
rockdragon <moyerock@gmail.com>
2014-11-18 14:09:47 +0800

    introduce Travis-CI & mocha BDD.

---
+++ test/request.js
@@ -0,0 +1,28 @@
+var request = require('../index');
+var expect = require('expect.js');
+var fs = require('fs');
+require('should');
+
+
+describe('request scenario', function () {
+    it('test get method', function (done) {
+        request.get('http://www.moye.me/', {gzip:true}, function(err, res){
+            res.should.not.null;
+            done();
+        });
+    });
+
+    it('test download method', function (done) {
+        var filePath = 'avatar.jpg';
+        request.download(
+            'http://gravatar.com/avatar/84cbf739853b2e023328e2c0428d663c',
+            'http://en.gravatar.com/',
+            {gzip:true},
+            filePath,
+            function(err, res){
+                fs.existsSync(filePath).should.equal(true);
+                done();
+            });
+    });
+});
+
